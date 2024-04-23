04b61804a85ef502cbafebc6037e9f3800dc06e4
Gabriel Bressi da Silva <gabriel.silva3@br.ey.com>
2024-04-22 22:20:20 -0300

    Added exercise 01

---
+++ src/exercicios/Ex01.java
@@ -0,0 +1,34 @@
+package exercicios;
+
+public class Ex01 {
+    /*
+    * Cria uma variável para cada um dos tipos abaixo
+    *
+    * boolean
+    * char
+    * String
+    * byte
+    * short
+    * int
+    * long
+    * float
+    * double
+    *
+    * Em seguida imprima cada uma das variáveis na tela
+    * */
+    public static void main(String[] args) {
+        /*
+        * Exemplo variável
+        * <tipo> identificador = valor;
+        * */
+        int numero = 1;
+
+
+        /*
+        * Exemplo para imprimir
+        * System.out.println(variavel);
+        * */
+        System.out.println(numero);
+    }
+
+}
