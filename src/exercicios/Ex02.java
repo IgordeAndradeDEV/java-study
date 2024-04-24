4a3893853146b71c3232d0fa3340ba610ad91376
Gabriel Bressi da Silva <gabriel.silva3@br.ey.com>
2024-04-23 21:53:11 -0300

    added exo02

---
+++ src/exercicios/Ex02.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Ex02 {
+
+    /*
+     *  Escreva um programa que leia 2 números inteiros e imprima a soma dos dois
+     *  */
+    public static void main(String[] args) {
+        
+    }
+
+}
