271358a1d1d1b51195f2ee6374bbfeaeb857d570
Gabriel Bressi da Silva <gabriel.silva3@br.ey.com>
2024-04-24 10:26:14 -0300

    added ex 03 e 04

---
+++ src/exercicios/Ex03.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+public class Ex03 {
+
+    /*
+        Escreva um programa que leia 3 valoes:
+        dia , mês e ano
+        e imprima na tela de duas formas:
+
+        exemplo primeira forma:
+        dia 15 do 10 de 2024
+
+        exemplo segunda forma
+        15/10/2024
+
+    */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/exercicios/Ex04.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class Ex04 {
+
+    /*
+        Escreva um programa que leia 3 valores:
+        hora, minuto e segundo
+        e imprima na tela conforme o exemplo:
+
+        15:30:20
+    */
+    public static void main(String[] args) {
+
+    }
+
+}
