d93e0054d2b816043344d158839965273e7f57bc
Gabriel Bressi da Silva <gabriel.silva3@br.ey.com>
2024-04-23 21:57:34 -0300

    commented aritmetics simbols

--- src/exercicios/Ex02.java
+++ src/exercicios/Ex02.java
@@ -6,7 +6,15 @@ public class Ex02 {
      *  Escreva um programa que leia 2 números inteiros e imprima a soma dos dois
      *  */
     public static void main(String[] args) {
-        
+        // operadores atitiméticos
+
+        /*
+            +   ->  soma
+            -   -> subtração
+            *   -> multiplicação
+            /   -> divisão
+
+        */
     }
 
 }
