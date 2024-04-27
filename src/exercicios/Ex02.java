0cbf899767b4e68789d55ed3c4b252d6a45ccaa4
Igorandrade-dev <igor_6500@hotmail.com>
2024-04-27 00:23:10 -0300

    Add Ex02

--- src/exercicios/Ex02.java
+++ src/exercicios/Ex02.java
@@ -1,5 +1,7 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class Ex02 {
 
     /*
@@ -7,6 +9,24 @@ public class Ex02 {
      *  */
     public static void main(String[] args) {
         // operadores atitiméticos
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int numero = scan.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int numero2 = scan.nextInt();
+
+        System.out.println(numero + numero2);
+
+        
+
+
+
+
+
+
+
 
         /*
             +   ->  soma
