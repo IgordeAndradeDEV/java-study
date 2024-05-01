92108e96ccef2f33d10ce88d1369ba19ff024400
Igorandrade-dev <igor_6500@hotmail.com>
2024-05-01 13:19:49 -0300

    Add ex04

--- src/exercicios/Ex04.java
+++ src/exercicios/Ex04.java
@@ -1,5 +1,7 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class Ex04 {
 
     /*
@@ -10,6 +12,19 @@ public class Ex04 {
         15:30:20
     */
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a hora: ");
+        int hora = scan.nextInt();
+
+        System.out.println("Digite o minuto: ");
+        int minuto = scan.nextInt();
+
+        System.out.println("Digite o segundo ");
+        int segundo = scan.nextInt();
+
+        System.out.println(hora +":" + minuto +":" + segundo );
+
 
     }
 
